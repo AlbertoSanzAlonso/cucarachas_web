ebb250de7ffe2450d083f3c7c9a95d25ecee8c53
AlbertoSanzAlonso <sanz_alonso84@hotmail.com>
2026-05-01 20:36:28 +0200

    fix: allow CORS credentials and set CSRF trusted origins

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -152,3 +152,11 @@ REST_FRAMEWORK = {
 # CORS settings
 CORS_ALLOWED_ORIGINS = os.environ.get('CORS_ALLOWED_ORIGINS', 'http://localhost:5173 http://178.104.249.230').split()
 CORS_ALLOW_ALL_ORIGINS = DEBUG # Only in dev
+CORS_ALLOW_CREDENTIALS = True
+
+# CSRF settings
+CSRF_TRUSTED_ORIGINS = [
+    'https://api.cucarachasbarcelona.cat',
+    'https://cucarachasbarcelona.cat',
+    'http://localhost:5173',
+]
